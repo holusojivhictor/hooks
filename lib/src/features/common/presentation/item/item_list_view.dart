6e96dc45dcc5e8d05e633a6d3a64a88655360f53
holusojivhictor <holusojivhictor@gmail.com>
2023-05-19 23:21:40 +0100

    Add slidable library

--- lib/src/features/common/presentation/item/item_list_view.dart
+++ lib/src/features/common/presentation/item/item_list_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fadein/flutter_fadein.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hooks/src/features/common/domain/models/models.dart';
+import 'package:hooks/src/features/stories/application/stories_bloc.dart';
+import 'package:hooks/src/features/stories/domain/models/db/models.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ItemListView<T extends Item> extends StatelessWidget {
@@ -30,7 +35,28 @@ class ItemListView<T extends Item> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final child = ListView();
+    final child = ListView(
+      children: <Widget>[
+        ...items.map((T e) {
+          if (e is Story) {
+            final hasRead = context.read<StoriesBloc>().hasRead(e);
+            return <Widget>[
+              GestureDetector(
+                child: FadeIn(
+                  child: Slidable(
+                    enabled: true,
+                    child: Placeholder(),
+                  ),
+                ),
+              ),
+            ];
+          } else if (e is Comment) {
+          }
+
+          return <Widget>[Container()];
+        }).expand((List<Widget> element) => element),
+      ],
+    );
     return const Placeholder();
   }
 }
